40be370bc01ef724da987c3ec914fcb1578cccc8
fahimvalanchery <fahimvalanchery@gmail.com>
2019-07-23 18:08:16 +0530

    form submition in console

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,8 +1,41 @@
 import React, { Component } from 'react';
 
+import './Auth.css';
+
 class AuthPage extends Component {
+  constructor(props) {
+    super(props);
+    this.emailEl = React.createRef();
+    this.passwordEl = React.createRef();
+  }
+
+  submitHandler = event => {
+    event.preventDefault();
+    const email = this.emailEl.current.value;
+    const password = this.passwordEl.current.value;
+
+    if (email.trim().length === 0 || password.trim().length === 0) {
+      return;
+    }
+    console.log(email, password);
+  };
   render() {
-    return <h1>Auth</h1>;
+    return (
+      <form className="auth-form" onSubmit={this.submitHandler}>
+        <div className="form-control">
+          <label htmlFor="email"> EMail</label>
+          <input type="email" id="email" ref={this.emailEl} />
+        </div>
+        <div className="form-control">
+          <label htmlFor="password"> Password</label>
+          <input type="password" id="password" ref={this.passwordEl} />
+        </div>
+        <div className="form-actions">
+          <button type="submit">Submit</button>
+          <button type="button">Switch to signup</button>
+        </div>
+      </form>
+    );
   }
 }
 
