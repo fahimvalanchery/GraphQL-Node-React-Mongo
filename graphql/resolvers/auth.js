3b4a0517fb55747a5ae17275f82764faf1057813
fahimvalanchery <fahimvalanchery@gmail.com>
2019-07-23 15:40:01 +0530

    auth wrks

--- graphql/resolvers/auth.js
+++ graphql/resolvers/auth.js
@@ -7,6 +7,8 @@ const UserModel = require('../../models/user');
  */
 const bcrypt = require('bcryptjs');
 
+const jwt = require('jsonwebtoken');
+
 module.exports = {
   createUser: async args => {
     try {
@@ -26,5 +28,27 @@ module.exports = {
     } catch (error) {
       throw error;
     }
+  },
+  login: async ({ email, password }) => {
+    const user = await UserModel.findOne({ email: email });
+    if (!user) {
+      throw new Error('User does not exist');
+    }
+    const isEqual = await bcrypt.compare(password, user.password);
+    if (!isEqual) {
+      throw new Error('Password is incorrect');
+    }
+    const token = jwt.sign(
+      { userId: user.id, email: user.email },
+      'mysecretkey',
+      {
+        expiresIn: '1h'
+      }
+    );
+    return {
+      userId: user.id,
+      token: token,
+      tokenExpiration: 1
+    };
   }
 };
